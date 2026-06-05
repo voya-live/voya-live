2b1c1f5b23b66156a853bfe6027ca2dfcd9b6d27
Mohamed Alshafei <shafeicisco@gmail.com>
2026-06-05 11:54:08 +0300

    Add VIP level system

--- src/models/User.js
+++ src/models/User.js
@@ -49,6 +49,10 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 1,
     },
+    vipLevel: {
+  type: Number,
+  default: 0,
+},
 
     experience: {
       type: Number,

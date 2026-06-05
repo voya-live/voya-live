e756f40a59cc1d153acb125592aaf065930aa94d
Mohamed Alshafei <shafeicisco@gmail.com>
2026-06-05 11:10:35 +0300

    Add social and leaderboard backend

--- src/routes/leaderboard.js
+++ src/routes/leaderboard.js
@@ -1,23 +1,44 @@
 import express from "express";
+
 import User from "../models/User.js";
 
 const router = express.Router();
 
-router.get("/", async (_, res) => {
+/*
+TOP GIFTERS
+*/
+
+router.get("/gifters", async (_, res) => {
   try {
     const users = await User.find()
-      .sort({
-        experience: -1,
-        followers: -1,
-      })
-      .limit(50)
+      .sort({ totalSpent: -1 })
+      .limit(20)
       .select(
-        "name level experience followers"
+        "name level totalSpent followers"
       );
 
-    res.json({
-      users,
+    res.json(users);
+  } catch {
+    res.status(500).json({
+      error: "Leaderboard error",
     });
+  }
+});
+
+/*
+TOP FOLLOWERS
+*/
+
+router.get("/followers", async (_, res) => {
+  try {
+    const users = await User.find()
+      .sort({ followersCount: -1 })
+      .limit(20)
+      .select(
+        "name level followersCount totalSpent"
+      );
+
+    res.json(users);
   } catch {
     res.status(500).json({
       error: "Leaderboard error",
